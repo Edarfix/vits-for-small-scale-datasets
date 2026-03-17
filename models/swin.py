5d29402c8640af84c7f880e559703bd07e49ebb1
Edarfix <1edarfix1@gmail.com>
2026-03-17 23:07:11 +0100

    Correct depreciated code in projection_head.py and train_ssl.py

--- models/swin.py
+++ models/swin.py
@@ -20,8 +20,8 @@ import torch.distributed as dist
 
 from math import sqrt
 from functools import partial
-from timm.models.layers import DropPath, to_2tuple, trunc_normal_
-from timm.models.registry import register_model
+from timm.layers import DropPath, to_2tuple, trunc_normal_
+from timm.models import register_model
 
 class Mlp(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
